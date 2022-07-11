d51a597d60d522f0b5ad324472b80ad12cf25c0e
Ivan Camilo <ibarragan@unal.edu.co>
2022-07-10 23:25:35 -0500

    feat(auth.py): new functions load_logged_in_user() and login_required()

--- auth.py
+++ auth.py
@@ -63,3 +63,28 @@ def login():
         flash(error)
 
     return render_template('auth/login.html')
+
+
+@bp.before_app_request
+def load_logged_in_user():
+    user_id = session.get('user_id')
+
+    if user_id is None:
+        g.user = None
+    else:
+        db, c = get_db()
+        c.execute(
+            'select * from user where username = %s', (user_id,)
+        )
+        g.user = c.fetchone()
+
+
+def login_required(view):
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        if g.user is None:
+            return redirect(url_for('auth.login'))
+
+        return view(**kwargs)
+
+    return wrapped_view
