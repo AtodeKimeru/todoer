11ec3e1cde9aa7e9b8c5f8bdec2027f01c4c0724
Ivan Camilo <ibarragan@unal.edu.co>
2022-07-24 08:03:15 -0500

    advance in last feature

--- todo.py
+++ todo.py
@@ -21,10 +21,76 @@ def index():
 @bp.route('/create', methods=['GET', 'POST'])
 @login_required
 def create():
+    if request.method == 'POST':
+        description = request.form['description']
+        error = None
+
+        if not description:
+            error = 'Description is required.'
+
+        if error is not None:
+            flash(error)
+        else:
+            db, c = get_db()
+            c.execute(
+                'insert into todo (description, completed, created_by)'
+                ' values (%s, %s, %s)',
+                (description, False, g.user['id'])
+            )
+            db.commit()
+            return redirect(url_for('todo.index'))
+
     return render_template('todo/create.html')
 
 
-@bp.route('/update', methods=['GET', 'POST'])
+def get_todo(id):
+    db, c = get_db()
+    c.execute(
+        'select t.id, t.description, t.completed, t.created_by, t.created_at, u.username ' 
+        'from todo t join user u on t.created_by = u.id where t.id = %s',
+        (id,)
+    )
+
+    todo = c.fetchone()
+
+    if todo is None:
+        abort(404, f"The todo of id {id} doesn't exist")
+
+    return todo
+
+
+@bp.route('/<int:id>/update', methods=['GET', 'POST'])
 @login_required
-def update():
+def update(id):
+    todo = get_todo(id)
+
+    if request.method == 'POST':
+        description = request.form['description']
+        completed = True if request.form.get('completed') == 'on' else False
+        error = None
+
+        if not description:
+            error = 'Description is required'
+
+        if error is not None:
+            flash(error)
+        else:
+            db, c = get_db()
+            c.execute(
+                'update todo set description = %s, completed = %s '
+                'where id = %s',
+                (description, completed, id)
+            )
+            db.commit()
+
+            return redirect(url_for('todo.index'))
+
+
+    return render_template('todo/update.html', todo=todo)
+
+
+@bp.route('/<int:id>/delete', methods=['POST'])
+@login_required
+def delete():
     return ''
+
